b270759729a6ea098bd86d191c8dec53924741fa
Codes Oracle <94334497+Oracle247@users.noreply.github.com>
2023-06-06 03:14:41 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center bg-white">
         <Navbar />
 
         {/* Head */}
