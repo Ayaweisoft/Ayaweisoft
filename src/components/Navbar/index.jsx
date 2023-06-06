0aa5a99296827dc0a305d2c351efeb70eb1eeac4
Codes Oracle <94334497+Oracle247@users.noreply.github.com>
2023-06-06 03:31:20 +0100

    change bg to white

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,7 +4,7 @@ import Button from '../Button'
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 bg-white">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
